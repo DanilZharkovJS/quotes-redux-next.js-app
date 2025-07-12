f6579d43a12d109a18281e4f5084a136f9b515c9
Danil Zharkov <zharkov.danik.ua@gmail.com>
2025-07-12 13:03:17 -0600

    search page refactoring FINISH

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,14 +1,14 @@
 'use client'
 
 import InputFields from './components/InputFields'
-
+import QuotesList from './components/QuotesList'
 
 export default function Search() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start bg-gray-900 px-6 py-10 text-white">
       <h1 className="text-4xl font-extrabold mb-8">Search Quotes</h1>
       <InputFields />
-
+      <QuotesList />
     </div>
   )
 }
